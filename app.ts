97b950999c1a452c4c8ae467b0cd42701e680a9c
Park Jae Young <izocuna@gmail.com>
2023-01-07 18:05:55 +0900

    1

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+/// mix in
 interface Store {
   currentPage: number;
   feeds: NewsFeed[];
@@ -35,110 +36,161 @@ const store: Store = {
   feeds: [],
 };
 
-function getData<AjaxResponse>(url: string): AjaxResponse {
-  ajax.open('GET', url, false);
-  ajax.send();
+// function applyMixins(derivedCtor: any, baseCtors: any[]) {
+//   baseCtors.forEach((baseCtor) => {
+//     Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+//       const descriptor = Object.getOwnPropertyDescriptor(baseCtor.prototype, name);
 
-  return JSON.parse(ajax.response);
+//       if (descriptor) {
+//         Object.defineProperty(derivedCtor.prototype, name, descriptor);
+//       }
+//     });
+//   });
+// }
+
+class Api {
+  url: string;
+  ajax: XMLHttpRequest;
+  constructor(url: string) {
+    this.url = url;
+    this.ajax = new XMLHttpRequest();
+  }
+  protected getRequest<AjaxResponse>(): AjaxResponse {
+    this.ajax.open('GET', this.url, false);
+    this.ajax.send();
+    return JSON.parse(this.ajax.response);
+  }
 }
 
-function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
-  for (let i = 0; i < feeds.length; i++) {
-    feeds[i].read = false;
+class NewsFeedApi extends Api {
+  getData(): NewsFeed[] {
+    return this.getRequest<NewsFeed[]>();
+  }
+}
+class NewsDetailApi extends Api {
+  getData(): NewsDetail {
+    return this.getRequest<NewsDetail>();
+  }
+}
+
+class View {
+  template: string;
+  container: HTMLElement;
+
+  constructor(containerID: string, template: string) {
+    const isThereRootContainer = document.getElementById(containerID);
+
+    if (!isThereRootContainer) {
+      throw new Error('최상의 컨테이너가 없어서 앱 실행을 하지못합니다');
+    }
+
+    this.container = isThereRootContainer;
+    this.template = template;
   }
 
-  return feeds;
+  updateView(): void {
+    this.container.innerHTML = this.template;
+  }
 }
+class NewsFeedView extends View {
+  constructor(contaierId: string) {
+    super(contaierId, template);
+    const api = new NewsFeedApi(NEWS_URL);
+    let newsFeed: NewsFeed[] = store.feeds;
 
-function updateView(html: string): void {
-  container != null ? (container.innerHTML = html) : console.error('최상위 컨테이너가 없습니다.');
-}
-
-function newsFeed(): void {
-  let newsFeed: NewsFeed[] = store.feeds;
-  const newsList = [];
-  const paging = 9;
-  const pageCount =
-    newsFeed.length % paging === 0
-      ? Math.floor(newsFeed.length / paging)
-      : Math.floor(newsFeed.length / paging) + 1;
-  let template = `
-    <div class="bg-gray-600 min-h-screen">
-      <div class="bg-white text-xl">
-        <div class="mx-auto px-4">
-          <div class="flex justify-between items-center py-6">
-            <div class="flex justify-start">
-              <h1 class="font-extrabold">Hacker News</h1>
-            </div>
-            <div class="items-center justify-end">
-              <a href="#/page/{{__prev__page__}}" class="text-gray-500">
-                Previous
-              </a>
-              <a href="#/page/{{__next__page__}}" class="text-gray-500 ml-4">
-                Next
-              </a>
-            </div>
-          </div> 
+    let template = `
+      <div class="bg-gray-600 min-h-screen">
+        <div class="bg-white text-xl">
+          <div class="mx-auto px-4">
+            <div class="flex justify-between items-center py-6">
+              <div class="flex justify-start">
+                <h1 class="font-extrabold">Hacker News</h1>
+              </div>
+              <div class="items-center justify-end">
+                <a href="#/page/{{__prev__page__}}" class="text-gray-500">
+                  Previous
+                </a>
+                <a href="#/page/{{__next__page__}}" class="text-gray-500 ml-4">
+                  Next
+                </a>
+              </div>
+            </div> 
+          </div>
         </div>
-      </div>
-      <div class="p-4 text-2xl text-gray-700">
-        {{__news__feed__}}        
-      </div>
-  </div>
-  `;
+        <div class="p-4 text-2xl text-gray-700">
+          {{__news__feed__}}        
+        </div>
+    </div>
+    `;
 
-  if (newsFeed.length === 0) {
-    newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
-  }
-  for (let i = (store.currentPage - 1) * paging; i < store.currentPage * paging; i++) {
-    if (i < newsFeed.length) {
-      newsList.push(`
-        <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
-          <div class="flex">
-            <div class="flex-auto">             
-            <i class="fas fa-circle mr-1" style="
-            ${newsFeed[i].read ? 'color:red' : 'color:gray'}
-            "></i>
-              <a href="#/show/${newsFeed[i].id}">
-                ${newsFeed[i].title}
-              </a>  
-            </div>
-            <div class="text-center text-sm">
-              <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${
-                newsFeed[i].comments_count
-              }</div>
-            </div>
-          </div>
-          <div class="flex mt-3">
-            <div class="grid grid-cols-3 text-sm text-gray-500">
-              <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
-              <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
-              <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
-            </div>  
-          </div>
-        </div>    
-      `);
+    if (newsFeed.length === 0) {
+      newsFeed = store.feeds = makeFeeds(api.getData());
     }
   }
-  template = template.replace('{{__news__feed__}}', newsList.join(''));
-  template = template.replace(
-    '{{__prev__page__}}',
-    String(store.currentPage > 1 ? store.currentPage - 1 : store.currentPage)
-  );
-  template = template.replace(
-    '{{__next__page__}}',
-    String(store.currentPage < pageCount ? store.currentPage + 1 : pageCount)
-  );
 
-  updateView(template);
+  render(): void {
+    const newsList: any = [];
+    const paging: number = 9;
+    const pageCount: number =
+      newsFeed.length % paging === 0
+        ? Math.floor(newsFeed.length / paging)
+        : Math.floor(newsFeed.length / paging) + 1;
+    for (let i = (store.currentPage - 1) * paging; i < store.currentPage * paging; i++) {
+      if (i < newsFeed.length) {
+        newsList.push(`
+          <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+            <div class="flex">
+              <div class="flex-auto">             
+              <i class="fas fa-circle mr-1" style="
+              ${newsFeed[i].read ? 'color:red' : 'color:gray'}
+              "></i>
+                <a href="#/show/${newsFeed[i].id}">
+                  ${newsFeed[i].title}
+                </a>  
+              </div>
+              <div class="text-center text-sm">
+                <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${
+                  newsFeed[i].comments_count
+                }</div>
+              </div>
+            </div>
+            <div class="flex mt-3">
+              <div class="grid grid-cols-3 text-sm text-gray-500">
+                <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+                <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+                <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+              </div>  
+            </div>
+          </div>    
+        `);
+      }
+    }
+    template = template.replace('{{__news__feed__}}', newsList.join(''));
+    template = template.replace(
+      '{{__prev__page__}}',
+      String(store.currentPage > 1 ? store.currentPage - 1 : store.currentPage)
+    );
+    template = template.replace(
+      '{{__next__page__}}',
+      String(store.currentPage < pageCount ? store.currentPage + 1 : pageCount)
+    );
 
-  container != null ? container.innerHTML : console.error('최상위 컨테이너가 없습니다.');
+    updateView(template);
+
+    container != null ? container.innerHTML : console.error('최상위 컨테이너가 없습니다.');
+  }
+
+  makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
+    for (let i = 0; i < feeds.length; i++) {
+      feeds[i].read = false;
+    }
+    return feeds;
+  }
 }
 
-function newsDetail() {
-  const id: string = location.hash.substring(7);
-  const newsContent: NewsDetail = getData<NewsDetail>(CONTENT_URL.replace(':id', id));
-  let template = `
+class NewsDetailView extends View {
+  constructor() {
+    let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
     <div class="bg-white text-xl">
       <div class="mx-auto px-4">
@@ -166,41 +218,73 @@ function newsDetail() {
     </div>
   </div>
   `;
+  }
 
-  store.feeds.filter((value) => {
-    Number(value.id) === Number(id) && (value.read = true);
-  });
+  render() {
+    const id: string = location.hash.substring(7);
+    const api = new NewsDetailApi(CONTENT_URL.replace(':id', id));
+    const newsContent: NewsDetail = api.getData();
 
-  updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+    store.feeds.filter((value) => {
+      Number(value.id) === Number(id) && (value.read = true);
+    });
+    updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+  }
+
+  makeComment(comments: NewsComment[]): string {
+    const commentString = [];
+    for (let i = 0; i < comments.length; i++) {
+      const {
+        level,
+        time_ago,
+        user,
+        content,
+        comments: [],
+      } = comments[i];
+      commentString.push(`
+        <div style="padding-left: ${level * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${user}</strong> ${time_ago}
+          </div>
+           <p class="text-gray-700">${content}</p>
+        </div>    
+       `);
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments));
+      }
+    }
+    return commentString.join('');
+  }
 }
+// class Api {
+//   getRequest<AjaxResponse>(url: string): AjaxResponse {
+//     const ajax = new XMLHttpRequest();
+//     ajax.open('GET', url, false);
+//     ajax.send();
+
+//     return JSON.parse(ajax.response);
+//   }
+// }
+
+// class NewsFeedApi {
+//   getData(): NewsFeed[] {
+//     return this.getRequest<NewsFeed[]>(NEWS_URL);
+//   }
+// }
+// class NewsDetailApi {
+//   getData(id: string): NewsDetail {
+//     return this.getRequest<NewsDetail>(CONTENT_URL.replace(':id', id));
+//   }
+// }
+
+// interface NewsFeedApi extends Api {}
+// interface NewsDetailApi extends Api {}
+
+// applyMixins(NewsFeedApi, [Api]);
+// applyMixins(NewsDetailApi, [Api]);
 
 window.addEventListener('hashchange', router);
-
-function makeComment(comments: NewsComment[]): string {
-  const commentString = [];
-  for (let i = 0; i < comments.length; i++) {
-    const {
-      level,
-      time_ago,
-      user,
-      content,
-      comments: [],
-    } = comments[i];
-    commentString.push(`
-      <div style="padding-left: ${level * 40}px;" class="mt-4">
-        <div class="text-gray-400">
-          <i class="fa fa-sort-up mr-2"></i>
-          <strong>${user}</strong> ${time_ago}
-        </div>
-         <p class="text-gray-700">${content}</p>
-      </div>    
-     `);
-    if (comments[i].comments.length > 0) {
-      commentString.push(makeComment(comments[i].comments));
-    }
-  }
-  return commentString.join('');
-}
 
 function router(): void {
   const routePath = location.hash;
--- reame.md
+++ reame.md
@@ -1,34 +1,19 @@
-# HK App typescript 적용
+# MIXIN
 
-## 준비사항
+## Mixin function
 
-### 1) 확장자 js 파일을 ts 파일로 변경한다
+> 관계를 유연하게 가져갈 수 있고 다중 상속방식을 구현할 수 있다.(JS는 다중 상속이 안된다)
 
-### 2) tsconfig.json 파일을 root 에 생성한다
+```js
+function applyMixins(derivedCtor: any, baseCtors: any[]) {
+  baseCtors.forEach((baseCtor) => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+      const descriptor = Object.getOwnPropertyDescriptor(baseCtor.prototype, name);
 
-```json
-{
-  "compilerOptions": {
-    "strict": true,
-    "target": "ES5",
-    "module": "E55",
-    "alwaysStrict": true,
-    "noImplicitAny": true,
-    "noImplicitThis": true,
-    "sourceMap": true,
-    "downlevelIteration": true
-  }
+      if (descriptor) {
+        Object.defineProperty(derivedCtor.prototype, name, descriptor);
+      }
+    });
+  });
 }
 ```
-
-> strict : typescript 의 엄격성 적용
-
-> noImplicitAny : any 를 사용하지 못하고 명확하게 타입을 지정
-
-> target : JS 버전(ES5)
-
-> module : import, export 를 사용할지
-
-> sourceMap : dist 폴더(ts 컴파일러가 변환한 결과를 출력) 안의 ~.js.map 파일, ~js파일 을 매핑해주는 역할.
-> 디버깅 등 ts파일의 오류를 찾을 때 브라우저가 js파일을 ts파일과 매핑해서 쉽게 디버깅 하게 해준다
-> 개발환경과 실행환경을 동일하게 맞춰주는 설정
